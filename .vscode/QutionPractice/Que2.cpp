eae374271d93917d613ff5183ecefbf0b1115a7f
Ankita <bobdeankita24@gmail.com>
2024-10-13 03:16:27 -0230

    change somthing

--- .vscode/QutionPractice/Que2.cpp
+++ .vscode/QutionPractice/Que2.cpp
@@ -13,20 +13,20 @@
 // }
 //output:-sum = 210
 
-#include<iostream>
-using namespace std;
-int main ()
-{
-    int n = 20;
-    int sum;
-    for(int i=1;i<=n;i++)
-    {
-        sum = sum+i;
-        if(i == 5)
-        {
-            break;
-        }
-    }
-    cout<<"sum = "<<sum;
-}
-//output:-sum = 210
+// #include<iostream>
+// using namespace std;
+// int main ()
+// {
+//     int n = 20;
+//     int sum;
+//     for(int i=1;i<=n;i++)
+//     {
+//         sum = sum+i;
+//         if(i == 5)
+//         {
+//             break;
+//         }
+//     }
+//     cout<<"sum = "<<sum;
+// }
+// //output:-sum = 210
