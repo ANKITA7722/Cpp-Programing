a5e0f3bad3c6e637915d4486c8084daeb405600f
Kamlesh Shah <89266771+Kamlesh7049@users.noreply.github.com>
2024-10-13 01:19:36 -0230

    loops

---
+++ .vscode/QutionPractice/Que1.cpp
@@ -0,0 +1,43 @@
+// Que:- print number 1 to 5
+//#include<iostream>
+// using namespace std;
+// int main ()
+// {
+//     int count = 1;
+//     while(count <= 5) {
+//         cout<<count<<" ";
+//         count++;
+//     }
+//     cout<<"\n";
+// }
+//output:-1 2 3 4 5
+
+
+//
+// #include<iostream>
+// using namespace std;
+// int main ()
+// {
+//     int n = 20;
+//     int count = 1;
+//     while(count <= n) {
+//         cout<<count<<" ";
+//         count++;
+//     }
+//     cout<<"\n";
+// }
+// //output:- 1 2 3 4 5.................20.
+
+
+// #include<iostream>
+// using namespace std;
+// int main ()
+// {
+//     int n = 20;
+//     for(int i=1;i<=n;i++)
+//     {
+//         cout<<i;
+//     }
+// }
+//output:- 1 2 3 4 5.................20.
+
---
+++ .vscode/QutionPractice/Que2.cpp
@@ -0,0 +1,32 @@
+//sum of number from 1 to n ?
+// #include<iostream>
+// using namespace std;
+// int main ()
+// {
+//     int n = 20;
+//     int sum;
+//     for(int i=1;i<=n;i++)
+//     {
+//         sum = sum+i;
+//     }
+//     cout<<"sum = "<<sum;
+// }
+//output:-sum = 210
+
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int n = 20;
+    int sum;
+    for(int i=1;i<=n;i++)
+    {
+        sum = sum+i;
+        if(i == 5)
+        {
+            break;
+        }
+    }
+    cout<<"sum = "<<sum;
+}
+//output:-sum = 210
